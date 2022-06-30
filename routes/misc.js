5108fac16f8705d7f4f68f3f37defa3691211f22
Wilfred Almeida <almeidawilfred642@gmail.com>
2022-06-30 16:56:05 +0530

    submitFeedback API Done

--- routes/misc.js
+++ routes/misc.js
@@ -3,7 +3,8 @@ const { SUCCESS,INVALID_BODY,SERVER_ERROR}=require("../utils/constants")
 const router = require("express").Router()
 const authFunctions = require("../middleware/authFunctions")
 var fs = require('fs');
-const path = require("path")
+const path = require("path");
+const FeedbackModel = require("../models/FeedbackModel");
 
 
 router.post("/getBannerImage",
@@ -18,6 +19,32 @@ async(req,res)=>{
 
 }
 )
+
+
+router.post("/submitFeedback",
+authFunctions.authenticateUserToken,
+async(req,res)=>{
+
+    var subject = req.body.subject
+    var message = req.body.message
+
+    if(subject==undefined||subject==null||subject.length<10||message==undefined||message==null||message.length<10){
+        return res.status(INVALID_BODY).json({status:0,message:"Enter more than 10 characters"})
+    }
+
+    var feedback = new FeedbackModel({
+        userId:req.user.uid,
+        subject:subject,
+        message:message
+    })
+
+    feedback.save().then((r)=>console.log(r)).catch(e=>console.log(e));
+
+    return res.status(SUCCESS).json({status:1,message:"Data Saved Successfully",payload:null})
+
+}
+)
+
 
 function base64_encode(file) {
     // read binary data
